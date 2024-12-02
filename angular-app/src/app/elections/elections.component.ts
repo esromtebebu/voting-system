f3720b3489af52234cbcb2875bc72371ba0f97ac
esromtebebu <111666496+esromtebebu@users.noreply.github.com>
2024-12-02 23:33:56 +0100

    front end

---
+++ angular-app/src/app/elections/elections.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elections',
+  imports: [],
+  templateUrl: './elections.component.html',
+  styleUrl: './elections.component.css'
+})
+export class ElectionsComponent {
+
+}
---
+++ angular-app/src/app/voter/voter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voter',
+  imports: [],
+  templateUrl: './voter.component.html',
+  styleUrl: './voter.component.css'
+})
+export class VoterComponent {
+
+}
